c2b369db26e388e602f29152682bdd01eb690daa
owhiteman <54307171+owhiteman@users.noreply.github.com>
2022-09-08 07:57:30 +1000

    Sidebar nav UI

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -11,36 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Chats',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Account',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
+        title: const Text('Shared List'),
       ),
       body: Column(
         children: [
-          Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
+          const Center(
+            child: Text('Home'),
           ),
           TextButton(
               onPressed: () {
@@ -49,22 +29,59 @@ class _HomePageState extends State<HomePage> {
               child: const Text('Sign out'))
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'List',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        backgroundColor: Colors.blueAccent,
-        onTap: _onItemTapped,
+      drawer: const NavigationDrawer(),
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildHeader(context),
+            buildMenuItems(context),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget buildHeader(BuildContext context) {
+  return Container(
+    padding: EdgeInsets.only(
+      top: MediaQuery.of(context).padding.top,
+    ),
+  );
+}
+
+Widget buildMenuItems(BuildContext context) {
+  return Column(
+    children: [
+      ListTile(
+        leading: const Icon(Icons.group_outlined),
+        title: const Text('Group members'),
+        onTap: () {},
+      ),
+      const Divider(color: Colors.black54),
+      ListTile(
+        leading: const Icon(Icons.person_outline),
+        title: const Text('Profile'),
+        onTap: () {},
+      ),
+      ListTile(
+        leading: const Icon(Icons.logout_outlined),
+        title: const Text('Log out'),
+        onTap: () {
+          context.read<AuthService>().signOut();
+        },
+      )
+    ],
+  );
 }
